ec3e65fa5b76157b0114be2513004c718df46f45
mcyandy <lionel.zheng@163.com>
2016-10-09 23:30:14 +0800

    启动时将状态为2的任务状态改为1，并启动

--- src/com/jt/gateway/service/management/JobInit.java
+++ src/com/jt/gateway/service/management/JobInit.java
@@ -1,13 +1,17 @@
 package com.jt.gateway.service.management;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
+import org.jdom.JDOMException;
 
+import com.jt.bean.gateway.GwConfig;
 import com.jt.bean.gateway.JobInf;
 import com.jt.gateway.service.job.JobInfService;
+import com.jt.gateway.service.operation.GwConfigService;
 
 /**
  * 系统启动时需要调用的函数，启动jobinf表中所有状态不是-1的任务
@@ -18,6 +22,16 @@ public class JobInit {
 	private static Logger logger =Logger.getLogger(JobInit.class);
 
 	private JobInfService  jobService;
+	private GwConfigService configService;
+	public JobInit(){
+		try {
+			configService=new GwConfigService();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	public JobInfService getJobService() {
 		return jobService;
 	}
@@ -26,15 +40,31 @@ public class JobInit {
 	public void setJobService(JobInfService jobService) {
 		this.jobService = jobService;
 	}
-	public void init(){
+public void init(){
+		
 		List<JobInf> list=jobService.getAllJobs();
+		GwConfig config=null;
 		for(JobInf job:list){
-			if(job.getJobStatus()!=-1){
+			logger.info("尝试启动任务 jobName=["+job.getJobName()+"] jobid=["+job.getJobId()+"] status=["+job.getJobStatus()+"]");
+			config=configService.getConfig(job.getJobName());
+//			if(config==null){
+//				logger.info("jobName=["+job.getJobName()+"] 没有找到对应的配置信息，删除此任务");
+//				jobService.deleteTask(job,false);
+//				continue;
+//			}
+//			
+			if(job.getJobStatus()==2){
+				logger.info("jobName=["+job.getJobName()+"] 上次未正常结束，修改状态并启动");
+				jobService.setJobStatus(job.getJobId(), 2);
+				//updatetask会启动任务，因此continue即可
+				continue;
+			}
+			if(job.getJobStatus()==1){
 				try {
 					logger.info("启动任务：jobName=["+job.getJobName()+"] jobId=["+job.getJobId()+"]");
-					jobService.startSimJob(job.getJobId());
-				} catch (Exception e) {
-					e.printStackTrace();
+					jobService.startSimJob(job.getJobId()); 
+				} catch (Exception e) {  
+					e.printStackTrace(); 
 				}
 			}
 		}

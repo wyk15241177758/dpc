05fa459e1db9bb27ba9122439a2ff7aa3519c747
zxhjiutian <zxhjiutian@163.com>
2016-10-23 19:36:30 +0800

    关键词

--- src/com/jt/keyword/bean/KeyWord.java
+++ src/com/jt/keyword/bean/KeyWord.java
@@ -1,5 +1,55 @@
 package com.jt.keyword.bean;
 
 public class KeyWord {
+	public   Integer   id;
+	public   KeyWord  parent;
+	public   String    wordname;
+	public   String    extend;
+	public   Integer    floor;
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public KeyWord getParent() {
+		return parent;
+	}
+	public void setParent(KeyWord parent) {
+		this.parent = parent;
+	}
+	public String getWordname() {
+		return wordname;
+	}
+	public void setWordname(String wordname) {
+		this.wordname = wordname;
+	}
+	public String getExtend() {
+		return extend;
+	}
+	public void setExtend(String extend) {
+		this.extend = extend;
+	}
+	public Integer getFloor() {
+		return floor;
+	}
+	public void setFloor(Integer floor) {
+		this.floor = floor;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }

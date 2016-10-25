6765d0afd533975d2b04b05c31e13cb0141753f1
mcyandy <lionel.zheng@163.com>
2016-10-25 22:37:40 +0800

    10.25前端已添加回复功能，还需要少许微调

--- WebContent/qa/js/common.js
+++ WebContent/qa/js/common.js
@@ -56,7 +56,7 @@ $(function(){
   	if(mess == ""){
   		return false;
   	}else{
-  		$("#message").append("<div>" + mess + "</div>");
+  		//$("#message").append("<div>" + mess + "</div>");
   		$("#messCon").val("");
   		qaSearch(mess)
   	}
@@ -71,12 +71,59 @@ function qaSearch(question){
 			"begin":0,
 			"end":5};
 	$.getJSON("/QASystem/admin/qaSearch.do",param,function(data){
-		//console.log(data)
+		addQuestion(question);
+		addAnswer(data);
 		
 	})
 	
 }
-
+//追加问题
+function addQuestion(question){
+	var template="  <ul class='dialog'> <li class='right'></li> <li class='center'>{question}</li> <li class='left'></li> <li class='bottom'></li> </ul>";
+	template=template.replace(/\{question\}/g,question);
+	$("#message").append(template);
+}
+function addAnswer(qalist){
+	var template="<div class='dialog_tab'><div class='tab2_title'><ul>{categoryLi}</ul></div><div class='tab2_content'>{qaDiv}</div></div>";
+	
+	var answerArray=new Array();
+	//遍历分类，将答案放入对应分类的answerArray中
+	for(i=0;i<categoryArray.length;i++){
+		var curCategory={"name":categoryArray[i],"qa":new Array()};
+		for(j in qalist.msg){
+			if(categoryArray[i]==qalist.msg[j].category){
+				curCategory.qa.push(qalist.msg[j]);
+			}
+		}
+		if(curCategory.qa.length!=0){
+			answerArray.push(curCategory);
+		}
+	}
+	//遍历归类好的答案并显示
+	var categoryLi="";
+	var qaDiv="";
+	
+	for(i=0;i<answerArray.length;i++){
+		var curQa=""
+		//遍历这个分类下的答案
+		for(j=0;j<answerArray[i].qa.length;j++){
+			curQa+="<li><a href='"+answerArray[i].qa[j].url+"' target='_blank'>"+answerArray[i].qa[j].title+"</a></li>"
+		}
+		
+		//遍历分类
+		if(i==0){
+			categoryLi="<li class='slected2'>"+answerArray[i].name+"</li>"
+			qaDiv="<div class='container'> <ul class='news'> "+curQa+" </ul> </div>";
+		}else{
+			categoryLi+="<li>"+answerArray[i].name+"</li>"
+			qaDiv+="<div class='container hide'> <ul class='news'> "+curQa+" </ul> </div>";
+		}
+	}
+	
+	template=template.replace(/\{categoryLi\}/g,categoryLi);
+	template=template.replace(/\{qaDiv\}/g,qaDiv);
+	$("#message").append(template);
+}
 //横向tab选项卡
 function setTab(m,n){
 	var tli=document.getElementById("menu"+m).getElementsByTagName("li");

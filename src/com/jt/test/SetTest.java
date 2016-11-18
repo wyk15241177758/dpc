672bd10b1550f94c912570ef486cbcdc1e2fabda
mcyandy <lionel.zheng@163.com>
2016-11-18 17:49:51 +0800

    11

--- src/com/jt/test/SetTest.java
+++ src/com/jt/test/SetTest.java
@@ -6,9 +6,10 @@ import java.util.Set;
 public class SetTest {
 public static void main(String[] args) {
 	Set<String> set=new HashSet<String>();
-	set.add("政府");
-	set.add("创业");
-	set.add("政务知识");
+	set.add("1");
+	set.add("2");
+	set.add("3");
+	set.add("4");
 	for(String str:set){
 		System.out.println(str);
 	}

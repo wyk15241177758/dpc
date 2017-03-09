7f7d6996d8f7bec86b7bd53db03b64439a5a099f
mcyandy <lionel.zheng@163.com>
2017-03-09 11:14:52 +0800

    前端修改

--- WebContent/qa/js/common.js
+++ WebContent/qa/js/common.js
@@ -196,45 +196,62 @@ $(function() {
 			".addFavorite",
 			function() {
 				var question = $(this).attr("question");
-				var url = window.location.pathname + "index.html?question="
+				var url = window.location.href.split("?")[0];
+				if(url.indexOf("index.html")==-1){
+					url+="index.html";
+				}
+				url+="?question="
 						+ encodeURIComponent(question);
-				AddFavorite(url, "智能咨询平台问题：" + question);
+				addFavorite(url, "智能咨询平台问题：" + question);
+				$(".tipDiv2").css("display", "none");
 			})
 
 	// 设为首页绑定事件
+	$(document).on(
+		"click",
+		".addHome",
+		function() {
+			setHomepage()
+			$(".tipDiv2").css("display", "none");
+		})
 })
 
 // 定义加入收藏夹函数
-function AddFavorite(siteUrl, siteName) {
-	try {
-		if (document.all) {
-			window.external.addFavorite(siteUrl, siteName);
-		} else if (window.sidebar) {
-			window.sidebar.addPanel(siteName, siteUrl, '');
-		}
-	} catch (e) {
-		alert("加入收藏夹失败，请使用Ctrl+D快捷键进行添加操作!");
-	}
-}
+function addFavorite(siteUrl, siteName){  
+    if (document.all){  
+        try{  
+            window.external.addFavorite(siteUrl,siteName);  
+        }catch(e){  
+        	alert(e.message )
+            alert( "加入收藏失败，请使用Ctrl+D进行添加" );  
+        }  
+          
+    }else if (window.sidebar){  
+        window.sidebar.addPanel(document.title, window.location.href, "");  
+     }else{  
+        alert( "加入收藏失败，请使用Ctrl+D进行添加" );  
+    }  
+}  
+
 // 设为首页 <a onclick="SetHome(this,window.location)">设为首页</a>
-function SetHome(obj, vrl) {
-	try {
-		obj.style.behavior = 'url(#default#homepage)';
-		obj.setHomePage(vrl);
-	} catch (e) {
-		if (window.netscape) {
-			try {
-				netscape.security.PrivilegeManager
-						.enablePrivilege("UniversalXPConnect");
-			} catch (e) {
-				alert("此操作被浏览器拒绝！\n请在浏览器地址栏输入“about:config”并回车\n然后将 [signed.applets.codebase_principal_support]的值设置为'true',双击即可。");
-			}
-			var prefs = Components.classes['@mozilla.org/preferences-service;1']
-					.getService(Components.interfaces.nsIPrefBranch);
-			prefs.setCharPref('browser.startup.homepage', vrl);
-		}
-	}
-}
+function setHomepage(){  
+    if (document.all){  
+        document.body.style.behavior='url(#default#homepage)';  
+          document.body.setHomePage(window.location.href);  
+    }else if (window.sidebar){  
+        if(window.netscape){  
+            try{  
+                netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");  
+            }catch (e){  
+                alert( "该操作被浏览器拒绝，如果想启用该功能，请在地址栏内输入 about:config,然后将项 signed.applets.codebase_principal_support 值该为true" );  
+            }  
+        }  
+        var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components. interfaces.nsIPrefBranch);  
+        prefs.setCharPref('browser.startup.homepage',window.location.href);  
+    }else{  
+        alert('您的浏览器不支持自动自动设置首页, 请使用浏览器菜单手动设置!');  
+    }  
+}  
 
 function toggleDialog(obj, paramStatus) {
 	var toStatus = "";
